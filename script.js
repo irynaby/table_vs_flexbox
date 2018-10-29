b8b26e099e4455b08078f0ca1dd830e52cd67e31
Irina Patsihova <ipatsihova@eastbanctech.com>
2018-10-29 13:20:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 ;(function TimeThisPerformance() {
-    let start = new Date().getTime();
-    window.onload = () => {
-        setTimeout(() => {
-            let t = performance.timing;
+    var start = new Date().getTime();
+    window.onload = function() {
+        setTimeout(function() {
+            var t = performance.timing;
 
             console.log('Speed of selection is: ' + (t.loadEventEnd - t.responseEnd) + ' milliseconds');
 
-            let now = new Date().getTime();
-            let latency = now - start;
+            var now = new Date().getTime();
+            var latency = now - start;
             console.log('Page loading time: ' + latency + ' milliseconds');
 
-            let page_load_time = now - performance.timing.navigationStart;
+            var page_load_time = now - performance.timing.navigationStart;
             console.log('User-perceived page loading time: ' + page_load_time + ' milliseconds');
 
         }, 0);
 
-        let itemCountShowEl = document.getElementById('item-count');
-        let itemCount = document.getElementsByClassName('js-item').length;
+        var itemCountShowEl = document.getElementById('item-count');
+        var itemCount = document.getElementsByClassName('js-item').length;
 
         itemCountShowEl.innerHTML = itemCount;
     };
